1e433bd1d7c077be990caafbe440c7c575b5ea31
xavioo <rucahoxavi@gmail.com>
2022-01-27 11:51:01 +0200

    set getters and setters on entity

--- backend/react-spring-api/src/main/java/net/react/springboot/entity/Employee.java
+++ backend/react-spring-api/src/main/java/net/react/springboot/entity/Employee.java
@@ -1,5 +1,55 @@
 package net.react.springboot.entity;
 
-public class Employee {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "tbl_employees")
+public class Employee {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String name;
+	
+	private String location;
+	
+	private String department;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public String getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+		
 }
