38b8d10c5c65bbb7393dd032781230638d1b6147
xavioo <rucahoxavi@gmail.com>
2022-01-29 00:18:53 +0200

    start react app

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div>
+        <div>
+          <h1>Hello world</h1>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+
+export default App;
