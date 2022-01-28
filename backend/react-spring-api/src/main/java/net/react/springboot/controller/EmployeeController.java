af0a0a89963e5f8677887273c091af06935949cf
xavioo <rucahoxavi@gmail.com>
2022-01-28 15:56:50 +0200

    update an employee

--- backend/react-spring-api/src/main/java/net/react/springboot/controller/EmployeeController.java
+++ backend/react-spring-api/src/main/java/net/react/springboot/controller/EmployeeController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,6 +35,11 @@ public class EmployeeController {
 	@GetMapping("/employees/{id}")
 	public Employee getEmployeeById(@PathVariable Long id) {
 		return eRepo.findById(id).get();
+	}
+	
+	@PutMapping("/employees")
+	public Employee updateEmployee(@RequestBody Employee employee) {
+		return eRepo.save(employee);
 	}
 	
 	
