75e1a82fc41b44b20a7244451188cf9ef36bd74a
xavioo <rucahoxavi@gmail.com>
2022-01-28 15:32:01 +0200

    add an employee

--- backend/react-spring-api/src/main/java/net/react/springboot/controller/EmployeeController.java
+++ backend/react-spring-api/src/main/java/net/react/springboot/controller/EmployeeController.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +23,11 @@ public class EmployeeController {
 	@GetMapping("/employees")
 	public List<Employee> getAllEmployees() {
 		return eRepo.findAll();
+	}
+	
+	@PostMapping("/employees")
+	public Employee saveEmployeeDetails(@RequestBody Employee employee) {
+		return eRepo.save(employee);
 	}
 	
 	
