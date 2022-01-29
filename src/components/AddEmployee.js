dd555dfb62e770c4d7c0670790dfbeb9423016eb
xavioo <rucahoxavi@gmail.com>
2022-01-29 13:03:08 +0200

    create employee

--- src/components/AddEmployee.js
+++ src/components/AddEmployee.js
@@ -1,7 +1,32 @@
 import React from 'react';
+import { useState } from "react";
 import { Link } from "react-router-dom";
+// import { useHistory } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import employeeService from "../services/employee.service";
 
 const AddEmployee = () => {
+    const[name, setName] = useState('');
+    const[location, setLocation] = useState('');
+    const[department, setDepartment] = useState('');
+    // const history = useHistory();
+    const navigate = useNavigate();
+
+    const saveEmployee = (e) => {
+        e.preventDefault();
+        
+        const employee = {name, location, department};
+            //create
+            employeeService.create(employee)
+            .then(response => {
+             console.log("employee added successfully", response.data);
+              // history.push("/");
+              navigate('/')
+            })
+            .catch(error => {
+                console.log('something went wroing', error);
+            });
+        }
     
     return(
         <div className="container">
@@ -10,29 +35,40 @@ const AddEmployee = () => {
             <form>
                 <div className="form-group mb-2">
                     <input 
-                        type="text" 
-                        className="form-control col-4" id="name" placeholder="Enter name"
+                    type="text" 
+                    className="form-control col-4"
+                     id="name"
+                     value={name}
+                     onChange={(e) => setName(e.target.value)}
+                     placeholder="Enter name"
                     />
                 </div>
 
                 <div className="form-group mb-2">
                     <input 
-                        type="text" 
-                        className="form-control col-4"
-                        id="department"
-                        placeholder="Enter Department"
+                    type="text" 
+                    className="form-control col-4"
+                    id="department"
+                    value={department}
+                    onChange={(e) => setDepartment(e.target.value)}
+                    placeholder="Enter Department"
                     />
                 </div>
+
                 <div className="form-group mb-2">
                     <input 
-                        type="text" 
-                        className="form-control col-4"
-                        id="location"
-                        placeholder="Enter Location"
+                    type="text" 
+                    className="form-control col-4"
+                    id="location"
+                    value={location}
+                    onChange={(e) => setLocation(e.target.value)}
+                    placeholder="Enter Location"
                     />
                 </div>
-                <div >
-                    <button className="btn btn-primary mt-2">Save</button>
+
+                <div>
+                    {/* <button className="btn btn-primary mt-2">Save</button> */}
+                    <button onClick={(e) => saveEmployee(e)} className="btn btn-primary">Save</button>
                 </div>
             </form>
             <hr/>
@@ -40,5 +76,4 @@ const AddEmployee = () => {
         </div>
     )
 }
-
 export default AddEmployee;
